e0986c96756ea0dd6cd8bbdf93d3d3a09eee8002
Raphael <48337836+PhaellZX@users.noreply.github.com>
2021-12-14 21:41:53 -0300

    Add files via upload

---
+++ FString.py
@@ -0,0 +1,5 @@
+# USANDO F STRING
+nome = str(input('Digite o seu nome: '))
+idade = int(input('Digite a sua idade: '))
+altura = float(input('Digite a sua altura: '))
+print(f'Seu nome é {nome} \nSua idade é {idade} anos\nSua altura é {altura}cm')
