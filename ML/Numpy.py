ce99b68df2f29dfb724a5e762bf7dd794acd0375
Raphael <48337836+PhaellZX@users.noreply.github.com>
2022-06-17 19:41:52 -0300

    Add files via upload

---
+++ ML/Numpy.py
@@ -0,0 +1,20 @@
+# NUMPY
+import numpy as np
+
+a = np.array([1, 2, 3])
+print(a)
+
+# CRIANDO UMA MATRIZ
+
+b = np.array([(1, 5, 7),(5, 3, 9),(4, 6, 7)])
+print(b)
+
+# ADICIONANDO ZEROS ou UM(ONES) DENTRO DE UMA MATRIZ
+
+c = np.zeros((4,3))
+print(c)
+
+#MATRIZ COM 1 PELAS DIAGONAIS
+
+d = np.eye(10)
+print(d)
