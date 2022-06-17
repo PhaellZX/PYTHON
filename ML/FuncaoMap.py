4ae7abe8c9898b4488cfd7e8aec09d2fd2244081
Raphael <48337836+PhaellZX@users.noreply.github.com>
2022-06-17 19:41:24 -0300

    Add files via upload

--- ML/FuncaoMap.py
+++ ML/FuncaoMap.py
@@ -3,3 +3,11 @@ kmh = [40, 50, 56, 64, 73, 79, 85, 96, 100, 120]
 
 mph = list(map(lambda x: x/1.61, kmh))
 print(mph)
+
+# FUCAO LIST COMPREHENSION
+
+mph2 = [x/1.61 for x in kmh]
+print(mph)
+
+caracteres = [i for i in "Machine Learning"]
+print(caracteres)
