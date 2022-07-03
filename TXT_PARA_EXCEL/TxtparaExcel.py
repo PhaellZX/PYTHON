0b8aa3f61781d06dbe3c9be6f31743851e087d9e
Raphael <48337836+PhaellZX@users.noreply.github.com>
2022-07-03 12:31:05 -0300

    Add files via upload

---
+++ TXT_PARA_EXCEL/TxtparaExcel.py
@@ -0,0 +1,25 @@
+import csv
+import openpyxl
+from time import sleep
+
+input_file = str(input('Digite o nome do arquivo txt(colocando .txt): '))
+output_file = f'{input_file[:-4]}.xlsx'
+
+try:
+    with open(input_file,'r'):
+        wb = openpyxl.Workbook()
+        ws = wb.worksheets[0]
+
+        with open(input_file, 'r') as data:
+            reader = csv.reader(data, delimiter='\t')
+            for row in reader:
+                ws.append(row)
+
+        wb.save(output_file)
+        print('Arquivo txt convertendo para xls')
+        sleep(2)
+        print('ARQUIVO CONVERTIDO!!\n''PRESSSIONE QUALQUER BOTÃO...')
+        input()
+except IOError:
+    print('Arquivo não encontrado')
+    input()
